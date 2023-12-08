bcb07e294f8860f446d3f18a9ee9c1c9a74665d5
Akash Singh <developer.akash8@gmail.com>
2023-12-08 20:12:19 +0530

    rtc: handle sender connection

--- rtc.js
+++ rtc.js
@@ -1,8 +1,22 @@
+const rtc = new RTCPeerConnection();
+
 const type = document.querySelectorAll(".type div button");
 const copy = document.querySelector(".sdp button");
 const chatbox = document.querySelector(".chatbox");
-
+let flag = "";
 let connection = false;
+
+type[0].addEventListener("click", () => {
+  if (flag) {
+    return alert(`Already logged as ${flag}`);
+  }
+
+  type[0].style.backgroundColor = "rgb(255, 216, 168)";
+  copy.innerText = "copy sdp offer";
+  flag = "sender";
+
+  handleSender();
+});
 
 // function to add chat element to DOM
 const addChat = (message, type) => {
@@ -29,3 +43,68 @@ document.querySelector(".message button").addEventListener("click", () => {
   addChat(message.value, "sender");
   message.value = "";
 });
+
+// function to set sender's remote description
+const setRemoteDesc = (answer) => {
+  rtc
+    .setRemoteDescription(JSON.parse(answer))
+    .then((e) => {
+      console.log("Set sender remote description!!!");
+    })
+    .catch((e) => {
+      console.error("Error while setting sender remote description: ");
+      console.error(e);
+    });
+};
+
+// function to setup WebRTC connection at the sender's end
+const handleSender = async () => {
+  const dc = rtc.createDataChannel("channel");
+
+  dc.onmessage = (e) => {
+    console.log("Got Message:", e.data);
+    addChat(e.data, "receiver");
+  };
+
+  dc.onopen = (e) => {
+    console.log("Receiver Connected ");
+    copy.innerText = "connection established!";
+    connection = true;
+  };
+
+  const offers = [];
+
+  rtc.onicecandidate = (e) => {
+    offers.push(rtc.localDescription);
+  };
+
+  try {
+    const offer = await rtc.createOffer();
+    console.log("Created offer!");
+
+    await rtc.setLocalDescription(offer);
+    console.log("Set local description");
+  } catch (err) {
+    console.error(err);
+    console.error("Error while creating offer!");
+  }
+
+  copy.addEventListener("click", () => {
+    if (copy.innerText === "copy sdp offer") {
+      const offer = offers[offers.length - 1];
+      console.log("Offer copied :", offer);
+
+      navigator.clipboard.writeText(JSON.stringify(offer));
+      copy.innerText = "copied!!";
+
+      setTimeout(() => {
+        copy.innerText = "add answer sdp";
+      }, 1000);
+    } else {
+      const answer = prompt("Enter the answer sdp:");
+      if (answer.length > 50) setRemoteDesc(answer);
+    }
+  });
+
+  console.log("Offers or SDPs :", offers);
+};
